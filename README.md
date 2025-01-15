e03723a8ff255f2f0fc9d3b6e2c2998f5d61180a
Ian Blott <github@blott.com.au>
2025-01-15 11:12:01 +1100

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# com.carsmakingclouds.www
-com.carsmakingclouds.www
+#  carsmakingclouds.com WordPress Themes
+
+A wordpress theme for carsmakingclouds.com
+
+## Getting Started
+
+This is a modified version of the carzine wordpress theme. 
+
+Install the carzine folder in your wordpress /wp-content/themes/ folder
+
+### Prerequisites
+
+PHP, MySQL
+
+## Built With
+
+* [PHP](https://www.php.net)
+* [MySQL](https://www.mysql.com)
+* [JQuery](https://jquery.com)
+* [CSS]
+* [WAMP](https://www.mysql.com)
+
+## Contributing
+
+* **Ian Blott** - [blott.com.au](http://blott.com.au)
+
+## Authors
+
+* **Carzine** - [Carzine](https://wordpress.org/themes/carzine/)
+
+## License
+
+Copyright (C) Carzine - All Rights Reserved
+
+https://wordpress.org/support/theme/carzine/
+
+Please review Carzine's licencing for rules
---
+++ carzine/page-full.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Template Name: Fullwidth page
+ */
+get_header();?>
+    <div id="carzine_main_content_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="breadcrumbs_and_title">
+                        <h1><a title="<?php the_title_attribute();?>" href="<?php the_permalink();?>"><?php the_title();?></a></h1>
+
+                        <?php carzine_the_breadcrumb(); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="carzine_main_content_container">
+                        <?php get_template_part('theme_includes/single_page_area');?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+<?php get_footer();?>
